74a4891f5c4a4e1b3e7e8e4931e84fb9730f0190
王映絜 <amy890202.11@nycu.edu.tw>
2023-06-08 18:27:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,10 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python app_test/env_setting.py #python app_test/modify_api.py
-python bot/main.py
+python bot/main.py &
+
+# 等待一分鐘
+sleep 60
+
+# 停止Python腳本
+kill $(pgrep -f "python bot/main.py")
