c29d9faadd1ea4e41505c63d42e872f14e72a6c2
王映絜 <amy890202.11@nycu.edu.tw>
2023-06-08 18:07:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-python app_test/modify_api.py #python app_test/env_setting.py
+python app_test/env_setting.py #python app_test/modify_api.py
 python bot/main.py
