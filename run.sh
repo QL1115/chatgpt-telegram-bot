19f0aeb1d9a7eaeb31a761bdd7fcedd112100c29
王映絜 <amy890202.11@nycu.edu.tw>
2023-06-08 18:30:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ pip install -r requirements.txt
 python app_test/env_setting.py #python app_test/modify_api.py
 python bot/main.py &
 
-# 等待一分鐘
-sleep 60
+# 執行十分鐘
+sleep 600
 
 # 停止Python腳本
 kill $(pgrep -f "python bot/main.py")
